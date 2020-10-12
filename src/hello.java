7387e1b6dd31d22c139b9e50f5dd99c6a2c13946
khalil <elmaghraoui.khalil.1@gmail.com>
2020-10-12 16:24:41 +0100

    initial commit

---
+++ src/hello.java
@@ -0,0 +1,18 @@
+public class hello {
+
+    public static void main(String[] args)
+    {
+        byte A = 64;
+        short b= 2000;
+        int c= 45;
+
+        long som = 50000+10*(A+b+c);
+        System.out.println(som);
+
+
+
+
+
+    }
+}
+
