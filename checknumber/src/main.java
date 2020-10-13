b258a55a14a910f50f3c5b655eef74c45751707b
khalil <elmaghraoui.khalil.1@gmail.com>
2020-10-13 18:51:41 +0100

    Create main.java

---
+++ checknumber/src/main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+
+
+        long value =number.toMilesPerHour(10.5);
+        System.out.println("miles = "+value);
+
+        number.printconversion(10.5);
+
+    }
+}
