e9535a593e01b45932419ac97107f4d0cbef6647
khalil <elmaghraoui.khalil.1@gmail.com>
2020-10-13 18:54:13 +0100

    Create km_to_miles.java

---
+++ checknumber/src/km_to_miles.java
@@ -0,0 +1,42 @@
+public class km_to_miles {
+
+
+    public static void check(int checknumber)
+    {
+        if(checknumber>0)
+        {
+            System.out.println("the number is positive");
+        }
+        else if(checknumber<0)
+        {
+            System.out.println("the number is negative ");
+        }
+        else
+            System.out.println("the number is Null");
+    }
+
+    public static long toMilesPerHour(double KilometersPweHour) {
+        if(KilometersPweHour<= 0 )
+        {
+            return -1;
+        }
+        else {
+            return Math.round(KilometersPweHour/ 1.609);
+        }
+
+    }
+
+    public  static  void printconversion(double kmPerHours)
+    {
+        if (kmPerHours<=0)
+        {
+            System.out.println("invalid value ");
+        }
+        else
+        {
+            long milesPerHours= toMilesPerHour(kmPerHours) ;
+            System.out.println(kmPerHours + " km/h " + milesPerHours +"mi/h");
+
+        }
+    }
+}
