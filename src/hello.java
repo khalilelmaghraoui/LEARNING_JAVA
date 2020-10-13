302afc11cc4edcb84c97e622eb08d0c257d0c3fb
khalil <elmaghraoui.khalil.1@gmail.com>
2020-10-13 16:52:47 +0100

    Update hello.java

--- src/hello.java
+++ src/hello.java
@@ -6,11 +6,6 @@ public class hello {
         short b= 2000;
         int c= 45;
 
-        long som = 50000+10*(A+b+c);
-        System.out.println(som);
-        float cc = 5.4f;
-
-        boolean m= true;
 
         String mystring= "khalil elmaghroauoi ";
         System.out.println(mystring);
@@ -19,12 +14,6 @@ public class hello {
             System.out.println("you got the high score");
 
         calulatescore(true , 40,4,5);
-
-
-
-
-
-
 
     }
     public static void calulatescore(boolean GameOver,int score ,int levelComplet , int bonus  ){
@@ -35,9 +24,50 @@ public class hello {
             System.out.println("your final score is " + finalScore);
         }
 
+        int results = CaluclHighScorePostion(1500);
+        displayHighScoorePosition("khalil ", results);
+
+         results = CaluclHighScorePostion(900);
+        displayHighScoorePosition("maghroaui ", results);
+
+         results = CaluclHighScorePostion(400);
+        displayHighScoorePosition("samir ", results);
+
+         results = CaluclHighScorePostion(50);
+        displayHighScoorePosition("kamil ", results);
+
+    }
+
+
+    public static void displayHighScoorePosition(String name, int highscorepostion){
+
+        System.out.println(name + "managed to get into position " + highscorepostion + "on high score table ");
+
+
+    }
+
+    public static  int CaluclHighScorePostion(int PlayerScore){
+        if (PlayerScore>=1000)
+        {
+            return 1;}
+        else if (PlayerScore>=500 && PlayerScore <1000){
+            return 2;
+        }
+        else if (PlayerScore>=100 && PlayerScore<500 )
+        {
+            return 3;
+
+        }
+        else
+        {
+            return 4;
+        }
 
 
 
     }
+
+
+
 }
 
