ab83686e49551b1cd1f7b091bfbc37e19b3d35fa
khalil <elmaghraoui.khalil.1@gmail.com>
2020-12-27 22:37:38 +0100

    done

---
+++ Collections/src/com/Collection/MapProgram.java
@@ -0,0 +1,32 @@
+package com.Collection;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.lang.Integer.parseInt;
+
+public class MapProgram {
+    public static void main(String[] args) throws IOException {
+        Map<String,String> languages = new HashMap<String,String>();
+        languages.put("java","high compiled level OOP");
+        languages.put("Python","an interpreter , OOP");
+        languages.put("c","low level programming lng ");
+        languages.put("algorithm","an algorithmic language");
+        System.out.println(languages.get("c"));
+
+        for (String key:languages.keySet()) {
+            System.out.println(key +" : " + languages.get(key));
+
+        }
+        languages.remove("javaa");
+//        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+//        int n = parseInt(in.readLine());
+//        System.out.println(n);
+//
+
+
+    }
+}
